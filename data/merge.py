703a336f9bed6666dbc2a6363ba2169be97e1b32
Brendan Ward <bcward@consbio.org>
2019-01-17 15:32:54 -0800

    WIP: more work on data merge

--- data/merge.py
+++ data/merge.py
@@ -1,14 +1,22 @@
 import pandas as pd
+from feather import read_dataframe
 
 
 df = None
-
 years = range(2007, 2019)
 for year in years:
-    year_df = pd.read_csv("Echolocation Records - {} - Aggregate.csv".format(year))
-
+    year_df = pd.read_csv(
+        "data/src/Echolocation Records - {} - Aggregate.csv".format(year)
+    )
     if df is None:
         df = year_df
     else:
-        df = df.append(year_df)
+        df = df.append(year_df, sort=False, ignore_index=True)
+
+df = df.rename(columns={"x_coord": "x", "y_coord": "y"})
+
+
+# Convert height units to meters
+index = df.mic_ht_units == "feet"
+df.loc[index, "mic_ht"] = df.loc[index].mic_ht * 0.3048
 
