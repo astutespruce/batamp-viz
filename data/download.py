fb0c348a5a86ad6a5c0baa64cb0ebf2742b108c4
Brendan C. Ward <bcward@astutespruce.com>
2020-06-05 15:35:38 -0700

    Data update

--- data/download.py
+++ data/download.py
@@ -46,20 +46,21 @@ client.set_api_key(DATABASIN_USER, DATABASIN_KEY)
 ### Download activity datasets
 # these are known IDs added manually
 activity_dataset_ids = [
-    "f3ef386adb8f437483da50cda4250f04",
-    "37d8933946344eb3baff39f7f108c0c6",
-    "651a6494655443c6987002176b736dd3",
-    "98fc51abca8247288d2e054c0a71c018",
-    "be710420baf14602a154d3ccf2ceabbf",
-    "b05bc8c7e2ca4cd7941982cf3c2183e9",
-    "03c760fa91324b528c30555c9f5bb185",
-    "3759a8b3ffbe42789d1520b78a760d99",
-    "eb80ff8c422b4d2ea4e72ee44197e2d9",
-    "c75da33515eb4a8380845ab79c40e0f8",
-    "e74dd6ba43c84a09b16e105f5cd82374",
-    "95921d86cf5042d5b6905b57fd50b63d",
-    "4f057a7950b64cb18f6d7f4147503ffc",
-    "4ee2bb7e949644f19d131a601e7a0036",
+    "c708bee517044b1f921c96704f773d9d",  # 2003
+    "4f057a7950b64cb18f6d7f4147503ffc",  # 2006
+    "651a6494655443c6987002176b736dd3",  # 2007
+    "98fc51abca8247288d2e054c0a71c018",  # 2008
+    "03c760fa91324b528c30555c9f5bb185",  # 2009
+    "eb80ff8c422b4d2ea4e72ee44197e2d9",  # 2010
+    "c75da33515eb4a8380845ab79c40e0f8",  # 2011
+    "95921d86cf5042d5b6905b57fd50b63d",  # 2012
+    "e74dd6ba43c84a09b16e105f5cd82374",  # 2013
+    "3759a8b3ffbe42789d1520b78a760d99",  # 2014
+    "b05bc8c7e2ca4cd7941982cf3c2183e9",  # 2015
+    "be710420baf14602a154d3ccf2ceabbf",  # 2016
+    "37d8933946344eb3baff39f7f108c0c6",  # 2017
+    "f3ef386adb8f437483da50cda4250f04",  # 2018
+    "4ee2bb7e949644f19d131a601e7a0036",  # 2019
 ]
 
 download_datasets(client, activity_dataset_ids, activity_dir)
@@ -67,9 +68,21 @@ download_datasets(client, activity_dataset_ids, activity_dir)
 
 ### Download presence-only datasets
 presence_dataset_ids = [
-    "58bda89bcb10454c80312b609ee629e6",
-    "3bce959e8a914f35882806d667b3c126",
-    "eaec6b31f7d24273aa8de56590613134",
-    "6b210f6fac834705a0bbf998f1de3848",
+    "150574eb100a432f9f6c700e1ec1ca9f",  # 2001
+    "63c48bec830b4e9d9880fbf93616bfce",  # 2002
+    "527ea0cacaf34e189e35e67c44d63d20",  # 2006
+    "79450e0f18fb4199ae279a87dda3a0f1",  # 2007
+    "e0adb95ec90e45d8838b4388b3066083",  # 2008
+    "b431cb06275e4ff4b6b16f268ab5ae56",  # 2009
+    "5ae0b23e742a44408e70c91351377e32",  # 2010
+    "e130601f7b6b40819095868264a15404",  # 2011
+    "1a1b1c2755164a11b32c81d57b9989a2",  # 2012
+    "671c0d6136e14b229fa8a0f03eb52111",  # 2013
+    "9c093ead084d43c1a1ba7198672ff202",  # 2014
+    "fec641f2813746348b84366b1bf57687",  # 2015
+    "eaec6b31f7d24273aa8de56590613134",  # 2016
+    "3bce959e8a914f35882806d667b3c126",  # 2017
+    "58bda89bcb10454c80312b609ee629e6",  # 2018
+    "6b210f6fac834705a0bbf998f1de3848",  # 2019
 ]
 download_datasets(client, presence_dataset_ids, presence_dir)
