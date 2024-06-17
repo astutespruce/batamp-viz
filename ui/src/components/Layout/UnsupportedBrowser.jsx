4e1ae811930e15c4dc5a53b8eb9e99a95e8f5b4c
Brendan C. Ward <bcward@astutespruce.com>
2024-06-17 13:55:53 -0700

    Update JS / Python deps

--- ui/src/components/Layout/UnsupportedBrowser.jsx
+++ ui/src/components/Layout/UnsupportedBrowser.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 
-import { Box, Container, Flex, Heading } from 'theme-ui'
-import { ExclamationTriangle } from '@emotion-icons/fa-solid'
+import { Container, Heading } from 'theme-ui'
 
 const UnsupportedBrowser = () => (
   <Container>
-    <Flex sx={{ mt: '2rem', gap: '1rem' }}>
-      <Box sx={{ flex: '0 0 auto', color: 'grey.6' }}>
-        <ExclamationTriangle size="3rem" />
-      </Box>
-      <Heading as="h1" sx={{ flex: '1 1 auto' }}>
-        Unfortunately, you are using an unsupported browser.
-        <br />
-        <br />
-        Please use a modern browser such as Google Chrome, Firefox, or Microsoft
-        Edge.
-      </Heading>
-    </Flex>
+    <Heading as="h1" sx={{ flex: '1 1 auto' }}>
+      Unfortunately, you are using an unsupported browser.
+      <br />
+      <br />
+      Please use a modern browser such as Google Chrome, Firefox, or Microsoft
+      Edge.
+    </Heading>
   </Container>
 )
 
